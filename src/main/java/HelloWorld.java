0a4ff96dbc19fd36483ccd5c07fad980207e67dd
Dongjian Cai <dongjian.cai@hotmail.com>
2017-04-18 20:32:38 -0500

    play around with maven

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Simple Class
+ */
+public class HelloWorld {
+
+    /**
+     * Main method
+     * @param args input args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
